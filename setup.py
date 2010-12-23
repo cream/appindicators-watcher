0e8645d01d1c19e13686f00d1bf4f2dc00d2325a
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-12-23 21:39:39 +0100

    integrated contrib lib, extended setup.py, minor fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
-from cream.dist import setup
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
 
-setup('src/manifest.xml',
-    data_files = [
-        ('{module_dir}', ['src/watcher.py', 'src/manifest.xml'])
-        ]
-    )
+class post_install(install_scripts):
+
+    def run(self):
+        install_scripts.run(self)
+
+        from shutil import move
+        for i in self.get_outputs():
+            n = i.replace('.py', '')
+            move(i, n)
+            print "moving '{0}' to '{1}'".format(i, n)
+
+
+ID = 'org.cream.StatusNotifierWatcher'
+data_files = [('share/cream/{0}'.format(ID), ['src/manifest.xml'])]
+
+
+setup(
+    name = 'cream-appindicators',
+    version = '0.0.8',
+    author = 'The Cream Project (http://cream-project.org)',
+    url = 'http://github.com/cream/appindicators-watcher',
+    package_dir = {'appindicators': 'src/appindicators'},
+    packages = [
+        'appindicators',
+        'appindicators.dbusmenu',
+        'appindicators.host',
+        'appindicators.watcher'
+    ],
+    data_files = data_files,
+    cmdclass={'install_scripts': post_install},
+    scripts = ['src/appindicators-watcher.py']
+)
--- src/appindicators-watcher.py
+++ src/appindicators-watcher.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import cream
-from cream.contrib.appindicators.watcher import StatusNotifierWatcher
+from appindicators.watcher import StatusNotifierWatcher
 
 class WatcherModule(cream.Module, StatusNotifierWatcher):
     def __init__(self):
-        cream.Module.__init__(self)
+        cream.Module.__init__(self, 'org.cream.StatusNotifierWatcher')
         StatusNotifierWatcher.__init__(self)
 
 if __name__ == '__main__':
